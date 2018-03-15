433ccabfd080f4a5182298c884ed82471d3b8d07
Joanne Lee <vernapii@gmail.com>
2018-03-15 19:54:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,3 +14,20 @@ function destructivelyUpdateObjectWithKeyAndValue (object, key, value) {
 function deleteFromObjectByKey (object, key) {
   //deletes key from a clone of object and returns the new object
 }
+var recipes = new Object ({})
+
+var obj = { prop: 1 }
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({},obj,{[key]:value})
+}
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value
+  return object
+}
+function deleteFromObjectByKey(object, key) {
+  delete object.key;
+  return obj
+}
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object.key
+  return objec

1fe5522eda52618e1ed10111498c22e93516389e
Joanne Lee <vernapii@gmail.com>
2018-03-15 19:48:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
+var recipes = {}
 function object (recipes) {
   // defines a 'recipes' object
  
   typeofrecipes = object
+}
+function updateObjectWithKeyAndValue (object, key, value) {
+  //returns an object with the original key value pairs and the new key value pair
+  var obj = {};
+}
+function destructivelyUpdateObjectWithKeyAndValue (object, key, value) {
+  
+}
+function deleteFromObjectByKey (object, key) {
+  //deletes key from a clone of object and returns the new object
 }
